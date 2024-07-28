437b9b5079d7daad8cded437e2ef6704353473de
Aadarsh-shrivastava <aadarshkhurai@gmail.com>
2024-07-28 19:34:29 +0530

    fix:mongo_url

--- db/database.py
+++ db/database.py
@@ -1,4 +1,5 @@
 import motor.motor_asyncio
+import os
 
-client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017")
+client = motor.motor_asyncio.AsyncIOMotorClient(os.getenv('MONGO_URL',default='mongodb://localhost:27017'))
 db = client.image_processing_db
